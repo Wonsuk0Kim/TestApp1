700ac473e57fab74c8949efff0a7a515c4fc547c
Wonsuk Kim <wonsuk.kim@wowinfotech.co.kr>
2018-02-07 15:57:02 +0900

    Initial commit

---
+++ app/src/main/java/com/beyondsolution/testapp1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.beyondsolution.testapp1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView1.text = "Test App 1";
+    }
+}
